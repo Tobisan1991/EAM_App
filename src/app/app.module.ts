40d65e456fdef4d893c05ba5e34acab896640c6a
Daniel <d.koeber@online.de>
2017-11-20 21:30:36 +0100

    app Module angepasst
    - title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
-
+import { AppRoutingModule} from './app.routing';
 import { AppComponent } from './app.component';
 import { TutorialVideoComponent } from './tutorial-video/tutorial-video.component';
 import { ImageListComponent } from './image-list/image-list.component';
+import { TutorialVideoListComponent } from './tutorial-video-list/tutorial-video-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TutorialVideoComponent,
-    ImageListComponent
+    ImageListComponent,
+    TutorialVideoListComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
