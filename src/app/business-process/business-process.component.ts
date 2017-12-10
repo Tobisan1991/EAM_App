bfd9cac737ee7eb9f4229a6902b578fdf88f5cd4
Daniel <d.koeber@online.de>
2017-12-10 16:46:04 +0100

    bprocess EASY

--- src/app/business-process/business-process.component.ts
+++ src/app/business-process/business-process.component.ts
@@ -1,16 +1,42 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../data.service';
+import { Http } from '@angular/http';
+declare var firebase: any;
 
 @Component({
   selector: 'app-business-process',
   templateUrl: './business-process.component.html',
   styleUrls: ['./business-process.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  providers: [DataService]
 })
+
+
 export class BusinessProcessComponent implements OnInit {
 
-  constructor() { }
+  bfuncliste = [];
+  liste = [];
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    
+  this.fbGetBFuncData();
   }
+
+fbGetBFuncData(){
+  firebase.database().ref('/BFunctions/').orderByKey().on('child_added', (snapshot) => {
+    this.bfuncliste.push(snapshot.val())
+    
+  }
+)}
+
+fbPostData(name,descr, bfunc){
+  // firebase.database().ref('/BFunctions/').push({Descr: descr, Name: name});
+   firebase.database().ref().child('/BProcess/').child(name).set({
+     Name: name ,Descr: descr, Bfunc: bfunc
+     });
+ }
 
 }
