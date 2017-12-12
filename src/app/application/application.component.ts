b52093806de1a7cfb84fbb94cb743d256b079dd7
stelio92 <st.tzavelas@gmail.com>
2017-12-12 10:21:23 +0100

    paar alte sachen gelöscht

--- src/app/application/application.component.ts
+++ src/app/application/application.component.ts
@@ -1,32 +1,19 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { DbService } from '../db.service.service';
-import { NgForm } from '@angular/forms/src/directives/ng_form';
 
 @Component({
   selector: 'app-application',
   templateUrl: './application.component.html',
   styleUrls: ['./application.component.css'],
-  providers: [DbService],
   encapsulation: ViewEncapsulation.None
 })
 export class Application implements OnInit {
 
-  constructor(private _dbService:DbService) { }
-  name: string;
-  version: string;
-  fromTo: string;
-  description: string;
-  geography: string;
+  constructor() { }
+
   ngOnInit() {
   }
 
-  onSubmit(form : NgForm){
-    
-    this._dbService.addData(form.value)
-    .subscribe( data=>"")
-    
 
-  }
   
 
 }
