6e5ef29a53625d68c82a8e184c5bd8c407679941
Daniel <d.koeber@online.de>
2017-12-26 17:08:27 +0100

    clear fields

--- src/app/business-function/business-function.component.ts
+++ src/app/business-function/business-function.component.ts
@@ -3,6 +3,8 @@ import { Router, ActivatedRoute, Params} from '@angular/router';
 import { DataService } from '../data.service';
 import { Http } from '@angular/http';
 import { rootRoute } from '@angular/router/src/router_module';
+//import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators, Control} from '@angular/common';
+
 
 declare var firebase: any;
 
@@ -16,12 +18,20 @@ declare var firebase: any;
 })
 export class BusinessFunctionComponent implements OnInit {
 
-  liste = [];
-  bool = null;
   
+  name:String;
+  descr:String;
+  creationDate:String;
+  alterDate:String;
+  removeDate:String;
+  liste = [];
 
   constructor(private dataService: DataService,
-    private router: Router, private route: ActivatedRoute) { }
+    private router: Router, private route: ActivatedRoute) { 
+    //  this.creationDate=Date;
+      // this.alterDate=Date;
+      // this.removeDate=Date;
+  }
 
   ngOnInit() {
     
@@ -36,17 +46,17 @@ fbGetData(){
   //firebase.database().ref('/BFunctions/').orderByKey().on('child_added', (snapshot) => {
  // alter code ... neuer Code nimmt nur die Validen mit dem X Flag    
   this.liste.push(snapshot.val())
-  }
-  
-)
+  })
 }
 
 
 fbPostData(name,descr){
  // firebase.database().ref('/BFunctions/').push({Descr: descr, Name: name});
   firebase.database().ref().child('/BFunctions/').child(name).set({
-    Name: name ,Descr: descr, Flag: 'X'
+    Name: name ,Descr: descr, Flag: 'X'//, created: this.creationDate, altered: 'none', removed: 'none'
     });
+    this.name = '';
+    this.descr = '';      
 }
   // https://www.youtube.com/watch?v=Fb9o2uwRAk0 minute 3:24 
   //erklärung zu root verzeichnis etc.
@@ -62,7 +72,8 @@ refreshList(){
 
 fbDeleteData(key){
   firebase.database().ref().child('/BFunctions/').child(key).update({
-    Flag: 'Removed'});
+    Flag: 'Removed'//, removed: this.removeDate
+  });
     
 //   firebase.database().ref().child('/BFunctions/'+key+'/').remove(), 
    window.location.reload();
