7190dd62bfdd7b1f8ca9532f60d4bf35f097c8a6
stelio92 <st.tzavelas@gmail.com>
2018-01-14 20:54:33 +0100

    dynamisch: As-is to-be has-been
    mit ngif

--- src/app/application/application.component.ts
+++ src/app/application/application.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Router, ActivatedRoute, Params} from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { DataService } from '../data.service';
 import { Http } from '@angular/http';
 import { SearchNamePipe } from '../search-name.pipe'
 import { rootRoute } from '@angular/router/src/router_module';
-import {LoginComponent} from '../login/login.component'
+import { LoginComponent } from '../login/login.component'
 import { IMyOptions } from 'mydatepicker';
 
 
@@ -17,12 +17,12 @@ const d: Date = new Date();
   templateUrl: './application.component.html',
   styleUrls: ['./application.component.css'],
   encapsulation: ViewEncapsulation.None,
-  providers: [DataService, SearchNamePipe, LoginComponent ]
+  providers: [DataService, SearchNamePipe, LoginComponent]
 })
 export class Application implements OnInit {
 
-  name:String;
-  descr:String;
+  name: String;
+  descr: String;
   datum: String;
   typ: String;
   liste = [];
@@ -30,10 +30,13 @@ export class Application implements OnInit {
   column: string = 'Name';
   direction: number;
   loginName: String;
-  dateFrom:string;
-  dateTo:string;
-  geography:string;
-  version:string;
+  dateFrom: string;
+  dateTo: string;
+  geography: string;
+  version: string;
+  radio_asis: boolean;
+  radio_tobe: boolean;
+  radio_hasbeen: boolean;
 
   private myDatePickerOptions: IMyOptions = {
 
@@ -45,56 +48,75 @@ export class Application implements OnInit {
 
   constructor(
     private dataService: DataService,
-    private router: Router, 
+    private router: Router,
     private route: ActivatedRoute,
     private searchName: SearchNamePipe,
     private loginComponent: LoginComponent) {
-    
-      this.datum=Date().toString();
-     }
+
+    this.datum = Date().toString();
+  }
 
   ngOnInit() {
     this.fbGetData();
   }
 
-  fbGetData(){
+  fbGetData() {
 
     firebase.database().ref().child('/Application/').orderByChild('CFlag').equalTo('active').
-    on('child_added', (snapshot) => {  
-    //firebase.database().ref('/Appsystem/').orderByKey().on('child_added', (snapshot) => {
-   // alter code ... neuer Code nimmt nur die Validen mit dem X Flag    
-    this.liste.push(snapshot.val())
-    })
+      on('child_added', (snapshot) => {
+        //firebase.database().ref('/Appsystem/').orderByKey().on('child_added', (snapshot) => {
+        // alter code ... neuer Code nimmt nur die Validen mit dem X Flag    
+        this.liste.push(snapshot.val())
+      })
   }
 
-fbPostData(name,descr,dateFrom,dateTo, geography, version ){
-  console.log(name,descr, dateFrom,dateTo, geography);
-  // firebase.database().ref('/Appsystem/').push({Descr: descr, Name: name});
-  firebase.database().ref().child('/Application/').child(name).set({
-    AName: name ,BDescr: descr, CFlag: 'active', DCreationDate: this.datum, GDateFrom: dateFrom, HDateTo: dateTo, IGeography: geography, JVersion: version //, created: this.creationDate, altered: 'none', removed: 'none'
-  });
-  this.name = '';
-  this.descr = '';
-  this.dateFrom = '';
-  this.dateTo = '';
-  this.geography = '';
-  this.version = '';
-           
- }
+  fbPostData(name, descr, dateFrom, dateTo, geography, version) {
+    console.log(name, descr, dateFrom, dateTo, geography);
+    // firebase.database().ref('/Appsystem/').push({Descr: descr, Name: name});
+    firebase.database().ref().child('/Application/').child(name).set({
+      AName: name, BDescr: descr, CFlag: 'active', DCreationDate: this.datum, GDateFrom: dateFrom, HDateTo: dateTo, IGeography: geography, JVersion: version //, created: this.creationDate, altered: 'none', removed: 'none'
+    });
+    this.name = '';
+    this.descr = '';
+    this.dateFrom = '';
+    this.dateTo = '';
+    this.geography = '';
+    this.version = '';
 
- fbDeleteData(key){
-  firebase.database().ref().child('/Application/').child(key).update({
-    CFlag: 'archived'//, removed: this.removeDate
-  });
-//   firebase.database().ref().child('/BFunctions/'+key+'/').remove(), 
-   window.location.reload();
- }
+  }
 
- sort(property){
-  this.isDesc = !this.isDesc; //change the direction    
-  this.column = property;
-  this.direction = this.isDesc ? 1 : -1;
-};
+  fbDeleteData(key) {
+    firebase.database().ref().child('/Application/').child(key).update({
+      CFlag: 'archived'//, removed: this.removeDate
+    });
+    //   firebase.database().ref().child('/BFunctions/'+key+'/').remove(), 
+    window.location.reload();
+  }
+
+  sort(property) {
+    this.isDesc = !this.isDesc; //change the direction    
+    this.column = property;
+    this.direction = this.isDesc ? 1 : -1;
+  };
+
+  onRadioClick(val) {
+    if (val == 0) {
+      console.log(val);
+      this.radio_asis = true;
+      this.radio_tobe = false;
+      this.radio_hasbeen = false;
+    } else if (val == 1) {
+      console.log(val);
+      this.radio_asis = false;
+      this.radio_tobe = true;
+      this.radio_hasbeen = false;
+    } else if (val == 2) {
+      console.log(val);
+      this.radio_asis = false;
+      this.radio_tobe = false;
+      this.radio_hasbeen = true;
+    }
+  }
 
 
 }
