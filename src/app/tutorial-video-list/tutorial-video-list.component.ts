139e6641df84140b9c66dc792ee3e42c46f5ec62
Daniel <d.koeber@online.de>
2018-01-29 19:13:02 +0100

    asd

--- src/app/tutorial-video-list/tutorial-video-list.component.ts
+++ src/app/tutorial-video-list/tutorial-video-list.component.ts
@@ -1,44 +1,82 @@
-import { Input, Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Input, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { DataService } from '../data.service';
+import { Http } from '@angular/http';
+import { rootRoute } from '@angular/router/src/router_module';
+import { SearchNamePipe } from '../search-name.pipe';
+import { LoginComponent } from '../login/login.component';
+import {NavbarService} from '../navbar.service';
+
+declare var firebase: any;
 
 @Component({
   selector: 'app-tutorial-video-list',
   templateUrl: './tutorial-video-list.component.html',
-  styleUrls: ['./tutorial-video-list.component.css']
+  styleUrls: ['./tutorial-video-list.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  providers: [DataService, SearchNamePipe, LoginComponent]
 })
+
 export class TutorialVideoListComponent implements OnInit {
-  
-  @Input()
-  passedlink: string;
+ 
+  constructor(
+    private dataService: DataService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private searchName: SearchNamePipe,
+    private navbarService: NavbarService
+    ) {}
+
+
+  liste = [];
+  rootRef = firebase.database().ref();
+ 
+  ngOnInit() {
+
+    //     console.log(this.passedlink);
+      }
+
+      fbGetData() {
+
+        firebase.database().ref().child('/BFunctions/').orderByChild('CFlag').equalTo('active').
+          on('child_added', (snapshot) => {
+            //firebase.database().ref('/BFunctions/').orderByKey().on('child_added', (snapshot) => {
+            // alter code ... neuer Code nimmt nur die Validen mit dem X Flag    
+            this.liste.push(snapshot.val())
+          })}
+
+
+  // @Input()
+  // passedlink: string;
 
   
-  videoList = [{
-    // HIER KOMMT DANN DIE DATENBANK ANBINDUNG Z.B. REIN
-    name: "Einführungsvideo",
-    slug: "video-1",
-    embed:`6wD4V0rvlDI`,
-    text: "hier kommt der Text zu Tutorial 1"
-  },
-  {
-    name: "Fortgeschritten",
-    slug: "video-2",
-    embed: `nzyJ9imm29w`,
-    text: "hier kommt der Text zu Tutorial 2"
-  },{
-    name: "Perfection",
-    slug: "video-3",
-    embed: `lYvmbQiFnXE`,
-    text: "hier kommt der Text zu Tutorial 3"
-  } ];
-  constructor() { }
+  // videoList = [{
+  //   // HIER KOMMT DANN DIE DATENBANK ANBINDUNG Z.B. REIN
+  //   name: "Einführungsvideo",
+  //   slug: "video-1",
+  //   embed:`6wD4V0rvlDI`,
+  //   text: "hier kommt der Text zu Tutorial 1"
+  // },
+  // {
+  //   name: "Fortgeschritten",
+  //   slug: "video-2",
+  //   embed: `nzyJ9imm29w`,
+  //   text: "hier kommt der Text zu Tutorial 2"
+  // },{
+  //   name: "Perfection",
+  //   slug: "video-3",
+  //   embed: `lYvmbQiFnXE`,
+  //   text: "hier kommt der Text zu Tutorial 3"
+  // } ];
   
-    ngOnInit() {
+  
+  //   ngOnInit() {
 
-      console.log(this.passedlink);
-    }
+  //     console.log(this.passedlink);
+  //   }
 
-    getEmbedUrl(item){
-    return 'https://www.youtube.com/embed/' + item.embed
-    }
+  //   getEmbedUrl(item){
+  //   return 'https://www.youtube.com/embed/' + item.embed
+  //   }
 
 }
